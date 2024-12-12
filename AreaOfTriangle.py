7eb20619e55054cd7cea449cf0fc10c791d614fd
Vineet2511SRM <vs4622@srmist.edu.in>
2024-12-12 09:52:12 +0530

    Refute assignment

--- AreaOfTriangle.py
+++ AreaOfTriangle.py
@@ -15,7 +15,7 @@ Input: 5(base),10(height)
 Output: The area of triangle is: 25.0(Expected)
 
 Refute test case
-Input: -6(base),15(height)
-Expected output: The area of triangle cannot be negative
-Actual output: -45.0(Unexpected)
+Input: -6(base),15(height)/any string like "abc" or anything
+Expected output: "INVALID INPUT. PLEASE ENTER A POSITIVE FLOAT VALUE."
+Actual output: -45.0(Unexpected)/ValueError: could not convert string to float: 'abc'
 '''
