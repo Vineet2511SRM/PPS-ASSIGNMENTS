8c0ab69b0e0c70d0fbc03a92ae6bacd92abc2aa4
Vineet Seth <vs4622@srmist.edu.in>
2025-07-25 12:13:55 +0530

    Update AreaOfTriangle.py

--- AreaOfTriangle.py
+++ AreaOfTriangle.py
@@ -17,5 +17,5 @@ Output: The area of triangle is: 25.0(Expected)
 Refute test case
 Input: -6(base),15(height)/any string like "abc" or anything
 Expected output: "INVALID INPUT. PLEASE ENTER A POSITIVE FLOAT VALUE."
-Actual output: -45.0(Unexpected)/ValueError: could not convert string to float: 'abc'
+Actual output: -45.0(Unexpected)/ValueError: could not convert string to the float: 'abc'
 '''
